d665532b41f8107073275a0782b1802205321195
Pedro Afonso Paulina Ataide <150714429+Afonsope@users.noreply.github.com>
2026-04-05 21:08:58 -0300

    Add files via upload

---
+++ Estagiarios/src/Informacoes.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Informacoes {
+
+    ArrayList<Estagiario> listaEstagiarios = new ArrayList<>();   
+
+    public void maiorIdade(int idade){
+        
+    }
+}
